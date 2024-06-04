bdd44217e308f4fddfc029b2f8dec9cd6393ce58
hari anitha <hv8447@gmail.com>
2024-06-04 06:48:47 +0530

    Hello world tutorial

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Learn PHP</title>
+</head>
+
+<body>
+  <?php
+
+   // This is PHP learning course.
+  /**
+   * This is a multi line comment
+   * 2nd line of command.
+   */
+  echo ("Hello world");
+  echo "<br/>";
+  echo "<h1>Learn PHP</h1>"
+
+
+  ?>
+</body>
+
+</html>
